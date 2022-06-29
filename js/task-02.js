1b8372e51a2af470cc612c50e349a05c3d546241
Zina <zinasaenko@hotmail.com>
2022-06-29 16:23:39 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const liEl = document.createElement('li');
+  liEl.textContent = option;
+  liEl.classList.add('item');
+
+  return liEl;
+});
+
+ingredientsEl.append(...elements);
