189321029bb676e057a354adb16d2463bfe54a06
Zina <zinasaenko@hotmail.com>
2022-07-03 17:19:46 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+const handleSubmit = function (event) {
+  event.preventDefault();
+
+  const { email, password } = form.elements;
+  if (email.value === '' || password.value === '') {
+    alert('All fields are required');
+  }
+
+  if (email.value !== '' && password.value !== '') {
+    return (
+      console.log({ email: email.value, password: password.value }), event.currentTarget.reset()
+    );
+  }
+};
+
+form.addEventListener('submit', handleSubmit);
