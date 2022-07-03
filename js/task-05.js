447675e6249dd1f6e8a431a1566c4774c7948fb4
Zina <zinasaenko@hotmail.com>
2022-07-03 15:51:25 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const refs = {
+  input: document.querySelector('#name-input'),
+  output: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInputFocus);
+
+function onInputFocus(event) {
+  //   console.log(event.currentTarget.value);
+  if (event.currentTarget.value === '') {
+    refs.output.textContent = 'Anonymous';
+  } else {
+    refs.output.textContent = event.currentTarget.value;
+  }
+}
