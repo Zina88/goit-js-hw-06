ac584b97046d7991dca02cf3980036f543dd7dec
Zina <zinasaenko@hotmail.com>
2022-07-04 19:49:05 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,14 @@
-// const itemEl = document.querySelectorAll('.item');
-// console.log('Number of categories:', itemEl.length);
+const category = document.querySelector('#categories');
+console.log('Number of categories:', category.children.length);
+
+const itemEl = document.querySelectorAll('.item');
+
+itemEl.forEach(item => {
+  const titleEl = item.querySelector('h2');
+  console.log('Category:', titleEl.textContent);
+  const itemListEl = item.querySelectorAll('li');
+  console.log('Elements:', itemListEl.length);
+});
 
 // for (const item of itemEl) {
 //   const titleEl = item.querySelector('h2');
@@ -8,15 +17,26 @@
 //   console.log('Elements:', itemListEl.length);
 // }
 
-const category = document.querySelector('#categories');
-console.log('Number of categories:', category.children.length);
+// const category = document.querySelector('#categories');
+// console.log('Number of categories:', category.children.length);
 
-const itemEl = document.querySelectorAll('.item');
+// const itemEl = document.querySelectorAll('.item');
 
-const elements = [...itemEl].map(element => {
-  const titleEl = element.querySelector('h2');
-  console.log('Category:', titleEl.textContent);
+// const elements = [...itemEl].map(element => {
+//   const titleEl = element.querySelector('h2');
+//   console.log('Category:', titleEl.textContent);
 
-  const itemListEl = element.querySelectorAll('li');
-  console.log('Elements:', itemListEl.length);
-});
+//   const itemListEl = element.querySelectorAll('li');
+//   console.log('Elements:', itemListEl.length);
+// });
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
