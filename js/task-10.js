bd6f3fa249f01eb95d5e94196979a42ca0c08cfb
Zina <zinasaenko@hotmail.com>
2022-07-04 21:40:50 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -16,23 +16,8 @@ function createBoxes(amount) {
   let elementInList = ref.resultInnerValue.childElementCount;
 
   amount = ref.inputValueEl.value;
-  // console.log('amount: ', amount);
 
-  if (elementInList === 0) {
-    for (let i = 0; i < +amount; i += 1) {
-      ref.resultInnerValue.insertAdjacentHTML(
-        'beforeend',
-        `<div style="width: ${i * 10 + 30 + 'px'};
-          height: ${i * 10 + 30 + 'px'};
-          background-color: ${getRandomHexColor()};
-          ">
-          ${i + 1}
-        </div >`
-      );
-    }
-  }
-
-  if (elementInList !== 0) {
+  if (elementInList !== null) {
     for (let i = elementInList; i < +amount + elementInList; i += 1) {
       ref.resultInnerValue.insertAdjacentHTML(
         'beforeend',
@@ -40,12 +25,11 @@ function createBoxes(amount) {
           height: ${i * 10 + 30 + 'px'};
           background-color: ${getRandomHexColor()};
           ">
-          ${i + 1}
         </div >`
       );
     }
   }
-  ref.inputValueEl.value = 0;
+  ref.inputValueEl.value = amount;
 }
 
 function destroyBoxes() {
