8fd499c1079166a27b5fc31bb829dfeaade7acbd
Zina <zinasaenko@hotmail.com>
2022-07-04 21:04:28 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -9,34 +9,3 @@ itemEl.forEach(item => {
   const itemListEl = item.querySelectorAll('li');
   console.log('Elements:', itemListEl.length);
 });
-
-// for (const item of itemEl) {
-//   const titleEl = item.querySelector('h2');
-//   console.log('Category:', titleEl.textContent);
-//   const itemListEl = item.querySelectorAll('li');
-//   console.log('Elements:', itemListEl.length);
-// }
-
-// const category = document.querySelector('#categories');
-// console.log('Number of categories:', category.children.length);
-
-// const itemEl = document.querySelectorAll('.item');
-
-// const elements = [...itemEl].map(element => {
-//   const titleEl = element.querySelector('h2');
-//   console.log('Category:', titleEl.textContent);
-
-//   const itemListEl = element.querySelectorAll('li');
-//   console.log('Elements:', itemListEl.length);
-// });
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
